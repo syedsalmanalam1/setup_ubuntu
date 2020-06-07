a3c4f575e207dde82bc6beca3ca6ed809271e2cb
Salman <53822932+syedsalmanalam1@users.noreply.github.com>
2020-06-08 02:44:30 +0300

    docker with -y

--- setup_linux.sh
+++ setup_linux.sh
@@ -94,3 +94,31 @@ sudo apt-get install \
 echo "###### curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
 echo "##----------------------------------##"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+
+
+
+echo "###### sudo apt-key fingerprint 0EBFCD88"
+echo "##----------------------------------##"
+sudo apt-key fingerprint 0EBFCD88
+
+echo "###### sudo add-apt-repository \"
+echo "##----------------------------------##"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+echo "###### sudo apt-get update -y"
+echo "##----------------------------------##"
+sudo apt-get update -y
+
+echo "###### sudo apt-get install docker-ce docker-ce-cli containerd.io -y"
+echo "##----------------------------------##"
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+echo "###### apt-cache madison docker-ce"
+echo "##----------------------------------##"
+apt-cache madison docker-ce
+
+
